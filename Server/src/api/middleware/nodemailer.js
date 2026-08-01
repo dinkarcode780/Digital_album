c88223479e3558cae208bfb2c999dc7d0d8f0400
Dinkar Diwakar <dinkardiwakar0@gmail.com>
2026-08-01 20:56:09 +0530

    updated code

--- Server/src/api/middleware/nodemailer.js
+++ Server/src/api/middleware/nodemailer.js
@@ -10,8 +10,8 @@ const ipv4Lookup = (hostname, options, callback) => {
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
@@ -20,6 +20,7 @@ const transporter = nodemailer.createTransport({
   tls: {
     rejectUnauthorized: false,
   },
+  connectionTimeout: 10000,
 });
 
 export const sendResetPasswordEmail = async (email, otp) => {
